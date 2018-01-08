6e2cecae2820c4be200cc7d840dc872a63c30aa7
Joshua Swiss <swissjoshua@gmail.com>
2018-01-08 16:55:17 +0000

    basic menu options set up!

--- main-menu.js
+++ main-menu.js
@@ -9,7 +9,7 @@ function setMainMenu() {
   const template = [
     {
       label: isWindows ? 'File' : app.getName(),
-      subMenu: [
+      submenu: [
         {
           label: isWindows ? 'Exit' : `Quit ${app.getName()}`,
           accelerator: isWindows ? 'Alt+F4' : 'CmdOrCtrl+Q',
@@ -17,6 +17,19 @@ function setMainMenu() {
             app.quit();
           }
         }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        // does nothing, just a visual line
+        { role: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'selectall' },
       ]
     }
   ];
